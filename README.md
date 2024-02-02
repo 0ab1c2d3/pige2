b94a1047531c6cc60602ad6ed943d750229603a4
ceh <123@yopmail.com>
2024-02-02 09:57:04 -0500

    Stablement bugge

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# pige2
+# PowerShell Script: pige.ps1
+
+## Description
+Perform a random selection of duo. Duo are splut by line and items are split by comma. 
+
+## Usage
+
+### License
+This project is licensed under the [License Name] - see the LICENSE.md file for details.
+
+### Contributing
+If you would like to contribute to the project, please follow the Contributor Covenant.
+
+### Issues
+If you encounter any issues or have suggestions for improvements, please open an issue on the Issues page.
+
+### Parameters
+
+- `itemsFile` (mandatory): The file where the participants are stored
+- `outputFilePath` (mandatory): The draw output folder
+- `pairHistoryFolder` (optional): Folder for the draw history
+- `stats` (optional): Get the stats from the draw
+- `SendEmail` (optional): DISABLE - Function to send draw to participants
+
+### Example
+
+```powershell
+.\pige.ps1 -itemsFile (path) -outputFilePath  (path) -pairHistoryFolder (path)
+
+
+
+
+
--- pige.ps1
+++ pige.ps1
@@ -1,12 +1,21 @@
+# BUG majeur à fixer :P 
+# si le dernier pick a deja été pick,  ca loop à l'infinie. 
+# changer l'algorithme
+
+[CmdletBinding()]
+
+
 param (
     [string]$pairHistoryFolder,
     [string]$itemsFile,
-    [string]$outputFilePath
+    [string]$outputFilePath,
+    [bool]$stats = $false,
+    [bool]$SendEmail = $false
 
 )
 
 $DebugPreference = 'Continue'
-
+#$DebugPreference = "SilentlyContinue"
 
 #    [string]$emailServer = "your.smtp.server",
 #    [string]$emailFrom = "your@email.com"
@@ -34,7 +43,6 @@ function Send-Email {
     Send-MailMessage @smtpParams
 }
 
-
 if ($outputFilePath) {
     $outputFileName = "pige-$((Get-Date).ToString('yyyyMMdd-HHmmss')).csv"
     $outputFilePath = $outputFilePath + $outputFileName
@@ -46,13 +54,7 @@ if (Test-Path $outputFileName) {
     return
 }
 
-# Prompt the user for email credentials
-#$emailCredential = Get-Credential -Message "Enter your email credentials"
-
 # Initialize a list of items (in this case, human names)
-# Load items from the file
-
-#$items = @("Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Hannah", "Ivy", "Jack", "Katie", "Liam", "Mary", "Noah", "Olivia", "Peter", "Quinn", "Rachel", "Sam", "Tom", "Ursula", "Victor", "Wendy", "Xander", "Yvonne", "Zane")
 $items = @()
 
 if (-not $itemsFile){
@@ -64,25 +66,8 @@ if (-not $itemsFile){
     $items = $items -split ',' | ForEach-Object { $_.Trim() }
 }
 
-# Function to detect duplicates in a list
-function Test-Duplicates {
-    param (
-        [array]$list
-    )
-
-    $duplicates = $list | Group-Object | Where-Object { $_.Count -gt 1 } | ForEach-Object { $_.Group[0] }
-
-    if ($duplicates.Count -gt 0) {
-        Write-Error "Duplicate items found: $($duplicates -join ', ')"
-        return $true
-    } else {
-        return $false
-    }
-}
-
 # Initialize an array for tracking pair history
 $pairHistory = @()
-
 
 # Validate pairHistoryFolder parameter
 if ($pairHistoryFolder) {
@@ -99,118 +84,123 @@ if ($pairHistoryFolder) {
         $historyPairs = ""
 
         foreach ($file in $pairHistoryFiles) {
-            Write-host "FILE $file"
             $historyContent = Get-Content $file.FullName -Raw
-            Write-Host "GETTING CONTENT OF  $file " $historyContent
+            Write-debug "Getting content of $file"
             $historyPairs = $historyContent -split '\r?\n' | Where-Object { $_ -ne '' }
             foreach ($pair in $historyPairs) {
-                Write-host "La PIGE = " $pair
+                Write-debug $pair
             }
-
 
             # Add each pair to the $pairHistory array
             foreach ($pair in $historyPairs) {
                 $pairHistory += $pair
-                #Write-Host "Adding $pair to historyPairs"
             }
         }
         
     }     
 }
 
-#Write-host "LA PAIR HISTORY $historyPairs"
-#foreach ($pair in $pairHistory) {
-    #Write-Host "LA PAIR $pair"
-#}
-
-# Identify new items and remove items that no longer exist
-#$newItems = $items | Where-Object { "$_notin$pairHistory" -eq $null }
-#$removedItems = $pairHistory | Where-Object { "$_notin$items" -eq $null }
-
-# Remove removed items from the pair history
-#$pairHistory = $pairHistory | Where-Object { "$_notin$removedItems" -eq $null }
-
 # Convert $items to an ArrayList
 $items = [System.Collections.ArrayList]$items
 
 # Create pairs of items without repeating any item or previous pairs
-
 $pairsFinal = New-Object System.Collections.ArrayList 
-   
+
+function draw {
+
+    param (
+        [array] $pairHistory,
+        [System.Collections.ArrayList] $items,
+        [System.Collections.ArrayList] $pairsFinal
+    )
+
     Do {
-        
+        Write-debug "DRAW BEGINS **************************"
         Do {
             $randomIndex1 = Get-Random -Minimum 0 -Maximum $items.Count
             $randomIndex2 = Get-Random -Minimum 0 -Maximum $items.Count
+        } while ($randomIndex1 -eq $randomIndex2)
+        Write-Debug "RANDOM INDEX 1: $randomIndex1"
+        Write-Debug "RANDOM INDEX 2: $randomIndex2"
+
+
+        $item1 = $items[$randomIndex1]
+        $item2 = $items[$randomIndex2]
+        Write-Debug "Item1 $item1"
+        Write-Debug "Item2 $item2"
+        $thePair = $item1 + "," + $item2
+    
+        $item1 = $items[$randomIndex1]
+        $item2 = $items[$randomIndex2]
+        
+        $thePair = $item1 + "," + $item2
+        Write-Debug "THE PAIR: $thePair"
+        $thePairSingleElement = $thePair  -split ',' | Where-Object { $_ -ne '' }
+        $pairReverse = $thePairSingleElement[1] + "," + $thePairSingleElement[0]
+        if (($pairHistory  -notcontains $pairReverse) -or ($pairHistory -notcontains $thePair) ){
+            write-debug "The pair does not exist yet"
+        }else{
+            Write-debug "The pair exists"
         }
-   
-    while ($randomIndex1 -eq $randomIndex2 )
-    Write-Debug "RANDOM INDEX 1: $randomIndex1"
-    Write-Debug "RANDOM INDEX 2: $randomIndex2"
 
-    $item1 = $items[$randomIndex1]
-    $item2 = $items[$randomIndex2]
+        #write-host "(pairHistory  -contains pairReverse) = " ($pairHistory  -contains $pairReverse)
+        #write-host "(pairHistory -contains thePair)" ($pairHistory -contains $thePair)
 
-    Write-Debug "Item1 $item1"
-    Write-Debug "Item2 $item2"
-    $thePair = $item1 + "," + $item2
-       
-    Write-Debug "THE PAIR: $thePair"
-    Write-Debug $thePair.GetType()
-    $thePairSingleElement = $thePair  -split ',' | Where-Object { $_ -ne '' }
-    
-    
-    if ($thePairSingleElement -contains $thePair){
-        WRITE-HOST "$item1 et $item2 ont deja ete pige."
-    }else{
-        WRITE-HOST "$item1 et $item2 n'ont pas deja ete pige. AJOUT a l'historique"
-    }
+    } while (($pairHistory  -contains $pairReverse) -or ($pairHistory -contains $thePair) )
 
     $item1 = $item1 + ""
     $item2 = $item2 + ""
     $thePair = $thePair + ""
     
-
-    $pairsFinal.Add($thePair)
-    
-    $items.RemoveAt($randomIndex1) 
-    $items.RemoveAt($randomIndex2) 
-    
+    $pairsFinal.Add($thePair) > $null
     Write-Debug "RANDOM INDEX 2: $pairs"
     Write-Debug "ITEMS Fin: $items"
+    #############################################
+    $items.Remove($item1) 
+    $items.Remove($item2) 
+    #$items.RemoveAt($randomIndex1) 
+    #$items.RemoveAt($randomIndex2) 
+    #############################################
+    Write-debug "DRAW Ends **************************"
+}
 
-    } while ($items.Count -ge 2)
+
+Do {
+    draw -pairHistory $pairHistory -items $items -pairsFinal $pairsFinal 
+} while ($items.Count -ge 2)
     
 if ($outputFilePath){
     $pairsFinal | Out-File $outputFilePath
     Write-Host "Pairs exported to $outputFilePath."    
 }
 
-# Output the pairs and send emails
-#for ($i = 0 ; $i -lt  $pairs.Count; $i+=2){    
-   #$Write-Host "PAIR $($i/2+1): $($pairs[$i]) - $($pairs[$i+1])"
-   #if ($outputFilePath) {
-    #    $($pairs[$i]) + "," +  $($pairs[$i+1]) | Out-File -FilePath $outputFilePath -Append
-        #$pair | Out-File -FilePath $outputFilePath -Append
+# SEND EMAIL SECTION 
+if ($SendEmail -eq $true){
+    # Prompt the user for email credentials
+    #$emailCredential = Get-Credential -Message "Enter your email credentials"
+    #FONCTION À AJOUTER
+    # Output the pairs and send emails
+    #for ($i = 0 ; $i -lt  $pairs.Count; $i+=2){    
+    #$Write-Host "PAIR $($i/2+1): $($pairs[$i]) - $($pairs[$i+1])"
+    #if ($outputFilePath) {
+        #    $($pairs[$i]) + "," +  $($pairs[$i+1]) | Out-File -FilePath $outputFilePath -Append
+            #$pair | Out-File -FilePath $outputFilePath -Append
 
+            
+        #} else {
+        #    Write-Host "$($pairs[$i]), $($pairs[$i+1])"
+        #}
         
-    #} else {
-    #    Write-Host "$($pairs[$i]), $($pairs[$i+1])"
-    #}
-    
-#    # Sending emails to the owners of each item
-#    Send-Email -to "owner@$item1.com" -subject "Pairing Notification" -body "You are paired with $item2." -credential $emailCredential
-#    Send-Email -to "owner@$item2.com" -subject "Pairing Notification" -body "You are paired with $item1." -credential $emailCredential
+    #    # Sending emails to the owners of each item
+    #    Send-Email -to "owner@$item1.com" -subject "Pairing Notification" -body "You are paired with $item2." -credential $emailCredential
+    #    Send-Email -to "owner@$item2.com" -subject "Pairing Notification" -body "You are paired with $item1." -credential $emailCredential
+}
+#################################
 
-
-
-# Save the updated pair history to the input files in the folder
-#foreach ($file in $pairHistoryFiles) {
-#    $historyContent = $pairHistory | Where-Object { $_ -like "$($file.BaseName)*" }
-#    $historyContent | Set-Content $file.FullName
-#}
-
-# Display statistics
-#Write-Host "Total Pairs Generated: $($pairs.Count)"
-#Write-Host "Unique Items Paired: $($items.Count)"
-#Write-Host "Average Pairs per Item: $($pairs.Count / $items.Count)"
+# Display statistics section
+if ($stats -eq $true){
+    Write-host "Final pair $pairsFinal"
+    Write-Host "Total Pairs Generated: $($pairsFinal.Count)"
+    Write-Host "Unique Items Paired: $($pairsFinal.Count)"
+}
+#################################
